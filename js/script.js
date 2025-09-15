b306f547d7f8a0ca621fb291a686668773ca55d6
Sachin <sachinarya68@gmail.com>
2025-09-15 20:17:20 +0530

    Songs played successfully

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-console.log('Spotify');
+console.log('Welcome to my Own Spotify!');
 
 async function getSongs() {
     let a = await fetch("http://10.22.32.124:3000/assets/songs/")
     let response = await a.text()
-    console.log(response)
+    // console.log(response)
 
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -12,17 +12,23 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split("%5Csongs%5C")[1].split("320")[0])
+            songs.push(element.href.split("%5Cassets%5Csongs%5C")[1].split("320")[0])
         }
     }
     return songs
 }
 
+const playMusic = (track) => {
+    let audio = new Audio(track);
+    audio.play();
+}
+
 async function main() {
     //Get songs list
     let songs = await getSongs()
-    console.log(songs);
+    // console.log(songs);
 
+    //Show songs
     let songsUL = document.querySelector(".saved").getElementsByTagName("ul")[0]
     for (const song of songs) {
         songsUL.innerHTML = songsUL.innerHTML + `<li>
@@ -34,21 +40,20 @@ async function main() {
                                 </div>
                             </div>
 
-
                             <div class="playbuttonsvg">
                                 <img src="assets/img/playbutton.svg" alt="">
                             </div>
                         </li>`;
     }
 
-    //Play songs
-    var audio = new Audio(songs[0]);
-    audio.play();
+    let currentSong;
 
-    audio.addEventListener("loadeddata", () => {
-        let duration = audio.duration;
-        console.log(audio.duration, audio.currentSrc, audio.currentTime);
-        // The duration variable now holds the duration (in seconds) of the audio clip
+    Array.from(document.querySelector(".saved").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log("/assets/songs/" + (e.querySelector(".songinfo").firstElementChild.innerHTML + "- " + e.querySelector(".songinfo").lastElementChild.innerHTML.trim()).replaceAll(" ", "%20") + " 320 Kbps.mp3".replaceAll(" ", "%20"));
+
+            playMusic("/assets/songs/" + (e.querySelector(".songinfo").firstElementChild.innerHTML + "- " + e.querySelector(".songinfo").lastElementChild.innerHTML.trim()).replaceAll(" ", "%20") + " 320 Kbps.mp3".replaceAll(" ", "%20"));
+        })
     });
 }
 
